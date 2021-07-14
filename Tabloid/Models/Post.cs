88e7bc0c2a711532f0ead4f5d69608ee3ce4da05
dudley-codes <alexdudley@me.com>
2021-07-14 11:36:48 -0500

    added backend code to fetch posts

--- Tabloid/Models/Post.cs
+++ Tabloid/Models/Post.cs
@@ -12,7 +12,7 @@ namespace Tabloid.Models
         public string Content { get; set; }
         public string ImageLocation {get; set;}
         public DateTime CreateDateTime { get; set; }
-        public DateTime PublishDateTime { get; set; }
+        public DateTime? PublishDateTime { get; set; }
         public bool IsApproved { get; set; }
         public int CategoryId { get; set; }
         public int UserProfileId { get; set; }
---
+++ Tabloid/client/src/modules/postManager.js
@@ -0,0 +1,20 @@
+import { getToken } from './authManager'
+
+const baseUrl = '/api/post';
+
+export const getAllPublishedPosts = () => {
+  return getToken().then((token) => {
+    return fetch(baseUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${ token }`
+      }
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("An unknown error occorred while trying to fetch all posts");
+      }
+    });
+  });
+};
