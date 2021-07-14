ab1c9d95ebc553f0129842e27ddab6ef18c487a1
dudley-codes <alexdudley@me.com>
2021-07-14 15:31:16 -0500

    checking out for PR review

--- Tabloid/client/src/index.js
+++ Tabloid/client/src/index.js
@@ -11,20 +11,6 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-const currentUser = firebase.auth().currentUser;
-
-console.log('currentUser', currentUser)
-
-let currentUserId = '';
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    console.log('firebase user', user.uid)
-    currentUserId = user.uid;
-  }
-})
-
 
 ReactDOM.render(
   <App />,
