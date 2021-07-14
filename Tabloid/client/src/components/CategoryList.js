04e939a2e0207e08fc4da9700d0a46fabcfbd450
HPreast <preasths1@gmail.com>
2021-07-14 14:45:59 -0500

    Removed console logs and old code

--- Tabloid/client/src/components/CategoryList.js
+++ Tabloid/client/src/components/CategoryList.js
@@ -4,32 +4,17 @@ import { getAllCategories } from "../modules/categoryManager";
 
 const CategoryList = () => {
     const [categories, setCategories] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false);
 
     const getAll = () => {
         return getAllCategories()
             .then(res => setCategories(res));
     }
 
-    // const load = () => {
-    //     {
-    //         categories == null ?
-    //             setIsLoaded(false) :
-    //             setIsLoaded(true);
-    //     }
-    // }
-
     useEffect(() => {
         getAll();
-        // load();
     }, [])
 
-    console.log(categories);
-    // debugger
     return (
-        // {categories == null ?
-        //     <div>
-        //     </div> :
         <>
             <h2>Categories</h2>
             <div>
