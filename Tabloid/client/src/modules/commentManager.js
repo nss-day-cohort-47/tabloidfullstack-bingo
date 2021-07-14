9f03ba885b60b0315cdffda4ecfe87982589cbcb
Dakota <dakota.c.upchurch@gmail.com>
2021-07-14 14:50:39 -0500

    saving to check out teammate PR

--- Tabloid/client/src/modules/commentManager.js
+++ Tabloid/client/src/modules/commentManager.js
@@ -1,5 +1,21 @@
+import { getToken } from './authManager'
+
 const baseUrl = '/api/Comment';
 
 export const getCommentsByPostId = (id) => {
-    return fetch()
-}
+    return getToken().then((token) => {
+        return fetch(`${baseUrl}/${id}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            else {
+                throw new Error("An unknown error occurred while trying to fetch comments");
+            }
+        });
+    });
+};
