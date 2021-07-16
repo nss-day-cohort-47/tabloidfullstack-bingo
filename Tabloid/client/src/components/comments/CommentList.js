8fc72ca2d84c588e3a5f865ddbe215a43562443e
Traye <johnson.traye@yahoo.com>
2021-07-16 15:21:23 -0500

    fixed more merge conflicts

--- Tabloid/client/src/components/comments/CommentList.js
+++ Tabloid/client/src/components/comments/CommentList.js
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCommentsByPostId } from "../../modules/commentManager";
-<<<<<<< HEAD
-import Post from "../post/Post";
-
-// Display all comments related to the Post
-const CommentList = ({ post }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = () => {
-    return getCommentsByPostId(post.id).then(res => setComments(res));
-=======
 import { getPostById } from "../../modules/postManager";
 import Post from "../post/Post";
 import Comment from "../comments/Comment";
@@ -29,23 +19,12 @@ const CommentList = () => {
 
   const fetchPosts = () => {
     return getPostById(id).then(res => setPost(res));
->>>>>>> main
   }
 
   useEffect(() => {
     fetchComments();
   }, []);
 
-<<<<<<< HEAD
-  return (
-    <>
-      <h2>{post.title}</h2>
-      <div className="container">
-        <div className="row justify-content-center">
-          {/* {comments.map((comment) => (
-                        <Comment comment={comment} key={comment.id} />
-                    ))} */}
-=======
   useEffect(() => {
     fetchPosts();
   }, []);
@@ -59,7 +38,6 @@ const CommentList = () => {
           {comments.map((comment) => (
             <Comment comment={comment} key={comment.id} post={post} />
           ))}
->>>>>>> main
         </div>
       </div>
     </>
